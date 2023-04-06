3dfe14748195828e2fe722167c6aef352bf34522
channnnni <github awdr5681@naver.com>
2023-04-06 18:11:17 +0900

    파일 업로드

---
+++ main.txt.js
@@ -0,0 +1,6 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js'
+    const swiper = new Swiper('.swiper-wrapper', {
+    slidesPerView : '1',
+    loop : true,
+})
+
